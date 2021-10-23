980fb59f6fa9b2cf4bcf2dd9422ca6fae368cc93
Sanavartak1427 <89457292+Sanavartak1427@users.noreply.github.com>
2021-10-23 21:37:20 +0530

    Add files via upload

---
+++ C-131/main.js
@@ -0,0 +1,18 @@
+function bedroom(){
+    window.location = "bedroom.html";
+}
+function fruitbasket(){
+    window.location = "fruitbasket.html";
+}
+function kitchen(){
+    window.location = "kitchen.html";
+}
+function hometheater(){
+    window.location = "hometheater.html";
+}
+function toys(){
+    window.location = "toys.html";
+}
+function wardrobe(){
+    window.location = "wardrobe.html";
+}
